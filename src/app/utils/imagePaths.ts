e0c8f1dbe7fec4431dfdc70bf986b068c81b33f0
tushar.bhatt <tushar.bhatt>
2025-05-04 01:28:36 +0530

    images paths resolved

--- src/app/utils/imagePaths.ts
+++ src/app/utils/imagePaths.ts
@@ -1,22 +1,22 @@
 // src/lib/assets.ts
 
 const IMAGES = {
-    gameQuestLogo : {src: '/images/GQ.png' , alt : 'gamequest logo'},
-    iosIcon : {src: '/icons/ios.svg' , alt : 'ios'},
-    windowsIcon : {src: '/icons/windows.svg' , alt : 'windows'},
-    rightArrowIcon : {src: '/icons/rightArrow.svg' , alt : 'Right Arrow'},
-    dropdownArrowIcon : {src: '/icons/dropdownArrow.svg' , alt : 'Dropdown Arrow'},
-    searchIcon : {src: '/icons/search.svg' , alt : 'search'},
-    notificationIcon : {src: '/icons/notification.svg' , alt : 'notification'},
-    shopIcon : {src: '/icons/shop.svg' , alt : 'shop'},
-    emptyCircleIcon : {src: '/icons/emptyCircle.svg' , alt : 'circle'},
-    settingsIcon : {src: '/icons/settings.svg' , alt : 'settings'},
-    logoutIcon : {src: '/icons/logout.svg' , alt : 'logout'},
+    gameQuestLogo : {src: '/Images/GQ.png' , alt : 'gamequest logo'},
+    iosIcon : {src: '/Icons/ios.svg' , alt : 'ios'},
+    windowsIcon : {src: '/Icons/windows.svg' , alt : 'windows'},
+    rightArrowIcon : {src: '/Icons/rightArrow.svg' , alt : 'Right Arrow'},
+    dropdownArrowIcon : {src: '/Icons/dropdownArrow.svg' , alt : 'Dropdown Arrow'},
+    searchIcon : {src: '/Icons/search.svg' , alt : 'search'},
+    notificationIcon : {src: '/Icons/notification.svg' , alt : 'notification'},
+    shopIcon : {src: '/Icons/shop.svg' , alt : 'shop'},
+    emptyCircleIcon : {src: '/Icons/emptyCircle.svg' , alt : 'circle'},
+    settingsIcon : {src: '/Icons/settings.svg' , alt : 'settings'},
+    logoutIcon : {src: '/Icons/logout.svg' , alt : 'logout'},
     twitterIcon : {src: '/SocialMediaIcons/twitter.svg' , alt : 'twitter'},
     facebookIcon : {src: '/SocialMediaIcons/facebook.svg' , alt : 'facebook'},
     instagramIcon : {src: '/SocialMediaIcons/instagram.svg' , alt : 'instagram'},
-    filledStar : {src: '/icons/filledStar.svg' , alt : 'filledStar'},
-    emptyStar : {src: '/icons/emptyStar.svg' , alt : 'emptyStar'},
+    filledStar : {src: '/Icons/filledStar.svg' , alt : 'filledStar'},
+    emptyStar : {src: '/Icons/emptyStar.svg' , alt : 'emptyStar'},
   };
   
 export default IMAGES;
